2ae5a13ba99358dff0c2af373d71b89f7640a024
Jamal AlYousif <jamal.alyousif@gmail.com>
2025-05-31 10:02:29 +0300

    Initial commit with working homepage

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Nasser Alyousif Collection</h1>
+      <p>Welcome to the archive of works.</p>
+    </div>
+  );
+}
